589bcecb8c3dfabbf35be5fa3a14042fc16118c6
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-02-20 23:41:51 +0900

    itemバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shopping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :price
+  end
+
+  validates :category_id, numericality: { other_than: 0 } 
+  validates :sales_status_id, numericality: { other_than: 0 } 
+  validates :shopping_fee_status_id, numericality: { other_than: 0 } 
+  validates :prefecture_id , numericality: { other_than: 0 } 
+  validates :scheduled_delivery_id, numericality: { other_than: 0 } 
+
 end
