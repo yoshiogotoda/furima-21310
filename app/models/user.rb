e7657e9c9aefc6fc7b15c6bdb5cfe2b6aac39dec
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-02-20 11:32:06 +0900

    モデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,presence: true
-  validates :last_name,presence: true
-  validates :first_name,presence: true
-  validates :last_name_kana,presence: true
-  validates :first_name_kana,presence: true
-  validates :birth_date,presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
+  with_options presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: ':＠ must be exit' } do
+    validates :email
+  end
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' } do
+    validates :password
+    validates :password_confirmation
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/i, message: ':全角で入力して下さい' } do
+    validates :first_name
+    validates :last_name
+  end   
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/i, message: ':全角カナで入力して下さい' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
---
+++ factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.initials(number: 6)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+  end
+end
