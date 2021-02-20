3e3ca53bfed5a94860bbfccbe689a7a4d8155878
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-02-20 23:43:34 +0900

    商品データ保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
+
+  def index
+   @items = Item.order("created_at DESC")
+  end
   def new
-   
+   @item = Item.new
+  end
+
+  def create
+   @item = Item.new(item_params)
+   if @item.save
+    redirect_to root_path
+   else
+    render :new
+   end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:explain,:category_id,:sales_status_id,:shopping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price).merge(user_id:current_user.id)
   end
 
 end
