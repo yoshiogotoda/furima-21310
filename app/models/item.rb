f83bf7b70373d2c9ae02a56aeb6a5ed68113cf16
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-02-22 18:17:22 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,11 +15,12 @@ class Item < ApplicationRecord
   belongs_to :shopping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
-
-  validates :category_id, numericality: { other_than: 0 } , inclusion: { in: 1..10 }
-  validates :sales_status_id, numericality: { other_than: 0 } , inclusion: { in: 1..6 }
-  validates :shopping_fee_status_id, numericality: { other_than: 0 } , inclusion: { in: 1..2 }
-  validates :prefecture_id , numericality: { other_than: 0 } , inclusion: { in: 1..47 }
-  validates :scheduled_delivery_id, numericality: { other_than: 0 } , inclusion: { in: 1..3 }
-
+  
+  with_options presence: true, numericality: { other_than: 0 } do
+    validates :category_id,            inclusion: { in: 1..10 }
+    validates :sales_status_id,        inclusion: { in: 1..6 }
+    validates :shopping_fee_status_id, inclusion: { in: 1..2 }
+    validates :prefecture_id ,         inclusion: { in: 1..47 }
+    validates :scheduled_delivery_id,  inclusion: { in: 1..3 }
+  end
 end
