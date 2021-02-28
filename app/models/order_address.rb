ac14743ee8b64d572c34b1e2f1e5007380bc6238
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-02-28 11:42:21 +0900

    order_addressモデルの修正（バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :item_id,:user_id,:postal_code,:prefecture_id,:city,:addresses,:building,:phone_number
+  attr_accessor :item_id,:user_id,:postal_code,:prefecture_id,:city,:addresses,:building,:phone_number,:token
   
   validates :postal_code, presence: true,format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
@@ -9,11 +9,12 @@ class OrderAddress
     validates :city
     validates :addresses
     validates :phone_number,format: { with: /\A[0-9]{11}\z/ ,message: "is invalid. Exclude hyphen(-)"}
+    validates :token, presence: true
   end
 
   def save
-    order = Order.create(user_id:current_user.id,item_id:item.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture, city: city, addresses: addresses, building: building,phone_number: phone_number,order_id: order.id)
+    order = Order.create(user_id:user_id,item_id:item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building,phone_number: phone_number,order_id: order.id)
   end
 
 end
