ddef27eacd487d6c864d10ceddf217324d492a3a
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-02-28 11:41:21 +0900

    カード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,11 @@ class OrdersController < ApplicationController
   end
 
   def create
+    
     @item = Item.find(params[:item_id])
-    binding.pry
-    @order_address = OrderAddress.new(order_params)   #「UserDonation」に編集
+    @order_address = OrderAddress.new(order_params)  
      if @order_address.valid?
+       payjp
        @order_address.save
        redirect_to root_path
      else
@@ -19,7 +20,16 @@ class OrdersController < ApplicationController
   private
    # 全てのストロングパラメーターを1つに統合
   def order_params
-   params.permit(:postal_code, :prefecture_id, :city, :addresses, :building,:phone_number).merge(user_id:current_user.id,item_id:params[:item_id])
+   params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building,:phone_number).merge(user_id:current_user.id,item_id:params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(order_params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
  
  end
