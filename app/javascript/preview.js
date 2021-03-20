dca91d756f1cb11ec5e86bccba97c1c3f5d92db4
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-03-20 16:32:41 +0900

    訂正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,4 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-<<<<<<< Updated upstream
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
 
@@ -25,31 +24,8 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
+
       createImageHTML(blob);
     });
   });
-=======
-document.addEventListener('DOMContentLoaded',function(){
-  const ImageList = document.getElementById('image-list');
-
-  const createImageHTML = (blob) => {
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-
-    blobImage.setAttribute('src',blob);
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
-  };
-  document.getElementById('item-image').addEventListener('change',function(e){
-    const imageContent = document.querySelector('img');
-    if (imageContent){
-      imageContent.remove();
-    }
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-
-    createImageHTML(blob);
-  });
-});
->>>>>>> Stashed changes
 }
