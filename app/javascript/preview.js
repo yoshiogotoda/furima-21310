c3fdde3feaf63d32050aecf19b1e24d655705ac0
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-03-20 16:21:20 +0900

    プレビュー機能の実装（再）

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,4 +1,5 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+<<<<<<< Updated upstream
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
 
@@ -28,4 +29,28 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       createImageHTML(blob);
     });
   });
+=======
+document.addEventListener('DOMContentLoaded',function(){
+  const ImageList = document.getElementById('image-list');
+
+  const createImageHTML = (blob) => {
+    const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img');
+
+    blobImage.setAttribute('src',blob);
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  };
+  document.getElementById('item-image').addEventListener('change',function(e){
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    createImageHTML(blob);
+  });
+});
+>>>>>>> Stashed changes
 }
