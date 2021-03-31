aa73cc1928add908508eeb246e18fd54006b1b2d
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-03-31 23:51:46 +0900

    Basic認証を導入

--- db/schema.rb
+++ db/schema.rb
@@ -82,8 +82,6 @@ ActiveRecord::Schema.define(version: 2021_02_27_081526) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
